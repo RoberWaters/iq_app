d8d723d8e44b85cd642e39a143371f3569563954
Ashley <silvasuarez249505@gmail.com>
2026-04-06 11:20:00 -0600

    Bat files to start the server

---
+++ run_backend.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "C:\Users\User\OneDrive\Escritorio\ingenieria\iq_app\lab-simulator\backend"
+uvicorn main:app --reload
+pause
---
+++ run_frontend.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "C:\Users\User\OneDrive\Escritorio\ingenieria\iq_app\lab-simulator\frontend"
+npm run dev
+pause
